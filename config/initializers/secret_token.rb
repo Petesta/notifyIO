b623e94317377bd58e679e8b55bb52fb3233c4ba
Mike Holubowski <mike.holubowski@gmail.com>
2013-10-17 00:15:04 -0700

    Downgrading Rails version to 3.9
    - used "rails _3.2.9_ new notifyIO"

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-NotifyIO::Application.config.secret_key_base = '6cfd97d6aba06ea7e8e3ee4f7495bca074506abeb5c9791a82902027114766d4b7fd0dd27611e83196e315227229b653b17e4cf8f64198307db5d9bd3d8a3938'
+NotifyIO::Application.config.secret_token = '13b4c88cf1f89d188d299fa0cdcad43469cb7d34550d27607ee403bbd3ac4046191209833d7062cd2330741713fc912d18b2f084efed39eb9d07e405babc738c'
