de10df584089cad0e861d9aa6075af8ef50a15f9
Mike Holubowski <mike.holubowski@gmail.com>
2013-10-17 01:57:12 -0700

    Mockups almost complete - integrated GIST in api_doc

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.9'
 gem 'sqlite3'
 
 gem 'pages'
+gem 'coderay', require: 'coderay'
 
 
 # Gems used only for assets and not required
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 NotifyIO::Application.routes.draw do
+  root to: "pages#splash_mock"
 
   # Static pages
   pages :splash_mock, :api_doc_mock, :login_mock, :signup_mock, :dashboard_mock, :settings_mock
