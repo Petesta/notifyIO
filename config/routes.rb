f522e5b43d592dd417b5d3531b892ef79cb3c312
Mike Sprague <mike@guidebook.com>
2013-10-28 15:30:47 -0700

    i'm going through channggeeeessss

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ NotifyIO::Application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :organizations
+  #resources :subscriptions
 
   root to: "pages#splash_mock"
 
   get "/organizations/:id/add_user/:user_email", to: "organizations#add_user"
-  post "/subscriptions/add_user", to: "subscriptions#add_user"
-
+#  get "/subscriptions/new", to: "subscriptions#add_user"
+  get "/subscriptions/add_user/:user_id/:organization_id", to: "subscriptions#add_user"
 
   # Static pages
   pages :splash_mock, :api_doc_mock, :login_mock, :signup_mock, :dashboard_mock, :settings_mock, :user_mock, :create_organization_mock
