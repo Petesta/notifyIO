83d3f3c85aa2aa5b6546a8a6781fb2c33626c638
Mike Holubowski <mike.holubowski@gmail.com>
2013-10-29 20:12:11 -0700

    Seed data for notifications + partial rendering v1

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ActiveRecord::Base
   belongs_to :organizations
 
-  attr_accessible :message, :organization_id, :type
+  attr_accessible :message, :organization_id, :notification_type
 end
