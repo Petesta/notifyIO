a60ab19b6064481198db8d947cf6f66b4197d4a6
Mike Sprague <mike@guidebook.com>
2013-10-29 20:33:56 -0700

    Basic test or two

--- test/unit/notification_test.rb
+++ test/unit/notification_test.rb
@@ -4,4 +4,13 @@ class NotificationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "new notification" do
+    notification = Notification.new
+    notification.message = "Test notification"
+    notification.notification_type = "SEVERE"
+    org = Organization.all
+    notification.organization_id = org.first.id
+    assert notification.save, "Save it"
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true
+   end
+
+  test "can't create user without email" do
+    user_test = User.new
+   assert !user_test.save, "Saved user without an email" 
+  end
+
+#  test "user should have an organization" do
+#    user = User.first
+#    assert_equal "NO", user.email
+#    assert_equal 1, user.organizations.count, "This user should have one organization"
+#  end
 end
