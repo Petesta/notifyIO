5b90efd084a1fe5435a74b8d437f7d0a871c78ea
Petesta <Petesta@live.com>
2013-10-30 13:29:57 -0700

    Fix the html and css for notifications

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,9 +1,9 @@
 class Organization < ActiveRecord::Base
-	has_many :subscriptions
-	has_many :users, through: :subscriptions
+  has_many :subscriptions
+  has_many :users, through: :subscriptions
   has_many :notifications
 
-	attr_accessible :description, :name, :subscription_id
+  attr_accessible :description, :name, :subscription_id
   validates :name, uniqueness: true
 
   before_create :set_api_key
