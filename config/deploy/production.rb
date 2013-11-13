17c511915751a056bab1a5876405d684a1fc0a24
Mike Holubowski <mike.holubowski@gmail.com>
2013-11-12 20:45:45 -0800

    Updating Capfile

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,7 +10,7 @@ set :rvm_ruby_version, '2.0.0-p247'
 # unless any hosts have the primary property set.
 #
 
-IP = '54.204.27.193'
+IP = '54-205-115-40'
 
 set :server_name, ["ec2-#{IP}.compute-1.amazonaws.com"]
 
